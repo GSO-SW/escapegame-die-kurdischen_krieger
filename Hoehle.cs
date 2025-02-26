527bba3dcc98498f0e3bc801a26086be93b928d1
Ehven Aydin <142978714+ehven62@users.noreply.github.com>
2025-02-26 10:41:44 +0100

    Klasse Höhle erstellt

---
+++ Hoehle.cs
@@ -0,0 +1,11 @@
+class Hoehle { 
+
+
+    public int HoehlenNr;
+	public bool CharackterAnw;
+	public bool Zugang;
+	public bool Goldspuren;
+	public bool Kreaturen;
+	public int[] NaechsteRaum = new int [2];
+	public bool XatarsGold;
+}
