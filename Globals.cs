ebf42fa492abac0ca6b6f902ec3cf6bd06484516
hawal_57 <142978648+hawal-57@users.noreply.github.com>
2025-04-02 09:58:01 +0200

    Create Globals.cs
    .

---
+++ Globals.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExitGame
+{
+    public static class Globals
+    {
+        public static int punkte = 0;
+        public static bool gameover = false;
+        public static Random random = new Random();
+        public static Hoehle aktuellerHoehle;
+    }
+}
