3dee928d2917112d2a37f891dd59e1e018ceb206
hawal_57 <142978648+hawal-57@users.noreply.github.com>
2025-04-30 18:59:38 +0200

    Update Navi.cs
    mm

--- Navi.cs
+++ Navi.cs
@@ -13,33 +13,36 @@ namespace ExitGame
     static class Navigation
     {
         //Methode zur Navigation, in welchen Raum die Person als nächstes gehen will
-        public static int Next()
+        public static void Next()
         {
             int naechsterHoehle = 0;
             Console.WriteLine("In welche Hohle möchtest du gehen? 1 = Höhle1, 2 = Höhle2, 3 = Höhle3, 4 = Höhle4");
+            string antwort = Console.ReadLine();
 
-            if (Console.ReadLine() == "1")
+            if (antwort == "1")
             {
-                naechsterHoehle = 1;
+                Globals.aktuelleHoehlenNr = 1;
+                
             }
-            else if (Console.ReadLine() == "2")
+            else if (antwort == "2")
             {
-                naechsterHoehle = 2;
-            }
-
-            else if (Console.ReadLine() == "3")
-            {
-                naechsterHoehle = 3;
+                Globals.aktuelleHoehlenNr = 2;
+                
             }
 
-            else if (Console.ReadLine() == "4")
+            else if (antwort == "3")
             {
-                naechsterHoehle = 4;
+                Globals.aktuelleHoehlenNr = 3;
+            }
+
+            else if (antwort == "4")
+            {
+                Globals.aktuelleHoehlenNr = 4;
             }
 
             else Console.WriteLine("Bitte geben Sie 1, 2, 3 oder 4 an.");
 
-            return naechsterHoehle;
+    
         }
 
     }
